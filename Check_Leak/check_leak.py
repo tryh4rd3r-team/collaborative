b03eac6f509f034e8e33d9b734e472406126aeda
Rival420 <51548322+Rival420@users.noreply.github.com>
2019-09-17 23:08:16 +0200

    v2.0
    merged dev into master branch. added some more additional help information

--- Check_Leak/check_leak.py
+++ Check_Leak/check_leak.py
@@ -5,9 +5,10 @@ import os
 import argparse
 import sys
 import re
+import textwrap
 
-
-#AVAILABLE SEARCH
+#
+#AVAILABLE SEARCHFields
 # - IP
 # - PassSalt
 # - UserId
@@ -21,53 +22,57 @@ import re
 
 
 HASH_TYPE_REGEX = {
-    re.compile(r"^[a-f0-9]{32}(:.+)?$", re.IGNORECASE):  ["MD5", "MD4", "MD2", "Double MD5",
-                                                          "LM", "RIPEMD-128", "Haval-128",
-                                                          "Tiger-128", "Skein-256(128)", "Skein-512(128",
-                                                          "Lotus Notes/Domino 5", "Skype", "ZipMonster",
-                                                          "PrestaShop"],
-    re.compile(r"^[a-f0-9]{64}(:.+)?$", re.IGNORECASE):  ["SHA-256", "RIPEMD-256", "SHA3-256", "Haval-256",
-                                                          "GOST R 34.11-94", "GOST CryptoPro S-Box",
-                                                          "Skein-256", "Skein-512(256)", "Ventrilo"],
-    re.compile(r"^[a-f0-9]{128}(:.+)?$", re.IGNORECASE): ["SHA-512", "Whirlpool", "Salsa10",
-                                                          "Salsa20", "SHA3-512", "Skein-512",
-                                                          "Skein-1024(512)"],
-    re.compile(r"^[a-f0-9]{56}$", re.IGNORECASE):        ["SHA-224", "Haval-224", "SHA3-224",
-                                                          "Skein-256(224)", "Skein-512(224)"],
-    re.compile(r"^[a-f0-9]{40}(:.+)?$", re.IGNORECASE):  ["SHA-1", "Double SHA-1", "RIPEMD-160",
-                                                          "Haval-160", "Tiger-160", "HAS-160",
-                                                          "LinkedIn", "Skein-256(160)", "Skein-512(160)",
-                                                          "MangoWeb Enhanced CMS"],
-    re.compile(r"^[a-f0-9]{96}$", re.IGNORECASE):        ["SHA-384", "SHA3-384", "Skein-512(384)",
-                                                          "Skein-1024(384)"],
-    re.compile(r"^[a-f0-9]{16}$", re.IGNORECASE):        ["MySQL323", "DES(Oracle)", "Half MD5",
-                                                          "Oracle 7-10g", "FNV-164", "CRC-64"],
-    re.compile(r"^\*[a-f0-9]{40}$", re.IGNORECASE):      ["MySQL5.x", "MySQL4.1"],
-    re.compile(r"^[a-f0-9]{48}$", re.IGNORECASE):        ["Haval-192", "Tiger-192", "SHA-1(Oracle)",
-                                                          "XSHA (v10.4 - v10.6)"]
+	re.compile(r"^[a-f0-9]{32}(:.+)?$", re.IGNORECASE):  ["MD5", "MD4", "MD2", "Double MD5",
+														  "LM", "RIPEMD-128", "Haval-128",
+														  "Tiger-128", "Skein-256(128)", "Skein-512(128",
+														  "Lotus Notes/Domino 5", "Skype", "ZipMonster",
+														  "PrestaShop"],
+	re.compile(r"^[a-f0-9]{64}(:.+)?$", re.IGNORECASE):  ["SHA-256", "RIPEMD-256", "SHA3-256", "Haval-256",
+														  "GOST R 34.11-94", "GOST CryptoPro S-Box",
+														  "Skein-256", "Skein-512(256)", "Ventrilo"],
+	re.compile(r"^[a-f0-9]{128}(:.+)?$", re.IGNORECASE): ["SHA-512", "Whirlpool", "Salsa10",
+														  "Salsa20", "SHA3-512", "Skein-512",
+														  "Skein-1024(512)"],
+	re.compile(r"^[a-f0-9]{56}$", re.IGNORECASE):        ["SHA-224", "Haval-224", "SHA3-224",
+														  "Skein-256(224)", "Skein-512(224)"],
+	re.compile(r"^[a-f0-9]{40}(:.+)?$", re.IGNORECASE):  ["SHA-1", "Double SHA-1", "RIPEMD-160",
+														  "Haval-160", "Tiger-160", "HAS-160",
+														  "LinkedIn", "Skein-256(160)", "Skein-512(160)",
+														  "MangoWeb Enhanced CMS"],
+	re.compile(r"^[a-f0-9]{96}$", re.IGNORECASE):        ["SHA-384", "SHA3-384", "Skein-512(384)",
+														  "Skein-1024(384)"],
+	re.compile(r"^[a-f0-9]{16}$", re.IGNORECASE):        ["MySQL323", "DES(Oracle)", "Half MD5",
+														  "Oracle 7-10g", "FNV-164", "CRC-64"],
+	re.compile(r"^\*[a-f0-9]{40}$", re.IGNORECASE):      ["MySQL5.x", "MySQL4.1"],
+	re.compile(r"^[a-f0-9]{48}$", re.IGNORECASE):        ["Haval-192", "Tiger-192", "SHA-1(Oracle)",
+														  "XSHA (v10.4 - v10.6)"]
 }
 
 
-def obtain_hash_type(check_hash):
-    found = False
-    for algorithm in HASH_TYPE_REGEX:
-        if algorithm.match(check_hash):
-            found = True
-            return enumerate_hash_types(HASH_TYPE_REGEX[algorithm])
-    if found is False:
-        return 0
-
-
-def enumerate_hash_types(items):
-    count = 0
-    for item in items:
-        count += 1
-        if count <= 1:
-            return format(item)
-
-
 def get_arguments():
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(
+		prog='Check_Leak.py',
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+		epilog=textwrap.dedent('''\
+		AVAILABLE SEARCH
+ 			- IP
+			 - PassSalt
+			 - UserId
+			 - Name
+			 - Email
+			 - User
+			 - Password
+			 - Username
+			 - PassHash
+			 - Domain
+		Additional Information:
+			Example search for passwords that start with ff
+			python check_leak.py -f Password -q ff*
+	This would match any passwords for username dave, dale, dane, etc.
+			python check_leak.py -f Name -q da?e
+	This would match any passwords with a d in them
+			python check_leak.py -f Password -q *d*
+	'''))
 	parser.add_argument("-f", "--field", dest="Field", help="Field to search in.")
 	parser.add_argument("-q", "--query", dest="Query", help="Query to look for")
 	parser.add_argument("-n", "--num", dest="Num", help="Number of results to display", default=100)
@@ -81,6 +86,24 @@ def get_arguments():
 	return options
 
 
+def obtain_hash_type(check_hash):
+	found = False
+	for algorithm in HASH_TYPE_REGEX:
+		if algorithm.match(check_hash):
+			found = True
+			return enumerate_hash_types(HASH_TYPE_REGEX[algorithm])
+	if found is False:
+		return 0
+
+
+def enumerate_hash_types(items):
+	count = 0
+	for item in items:
+		count += 1
+		if count <= 1:
+			return format(item)
+
+
 def set_para(options):
 	parameters = (
 		('q', options.Field + ":" + options.Query),
@@ -91,8 +114,8 @@ def set_para(options):
 
 
 def save_result(data_json):
-	filePath = Options.OutFile
-	sys.stdout = open(filePath, 'w')
+	pathtofile = Options.OutFile
+	sys.stdout = open(pathtofile, 'w')
 	for i in range(len(data_json)):
 		if 'Email' in data_json[i]["_source"]:
 			source = Red + "Email: " + Blue + data_json[i]["_source"]["Email"] + NC
@@ -121,12 +144,12 @@ def print_result(data_json):
 		if 'Password' in data_json[i]["_source"]:
 			data = Red + "Password: " + Blue + data_json[i]["_source"]["Password"] + NC
 			is_hash = obtain_hash_type(data_json[i]["_source"]["Password"])
-			if is_hash != 0 and is_hash != None:
+			if is_hash != 0 and is_hash is not None:
 				data += Red + "\n   Hash Type: " + Blue + is_hash + NC
 		elif 'PassHash' in data_json[i]["_source"]:
 			data = Red + "Hash: " + Blue + data_json[i]["_source"]["PassHash"] + NC
 			is_hash = obtain_hash_type(data_json[i]["_source"]["PassHash"])
-			if is_hash != 0 and is_hash != None:
+			if is_hash != 0 and is_hash is not None:
 				data += Red + "\n   Hash Type: " + Blue + is_hash + NC
 		if data_json[i]["_score"] >= 4:
 			print("   " + Red + "Domain: " + Blue + data_json[i]["_source"]["Domain"] + NC + "\n" + "   " + source + "\n" + "   " + data + "\n")
@@ -151,62 +174,13 @@ Options = get_arguments()
 
 params = set_para(Options)
 response = requests.get('https://scylla.sh/search', headers=headers, params=params)
-if Options.OutFile != False:
-    sys.stdout = open(Options.OutFile, 'w')
+if Options.OutFile is not False:
+	sys.stdout = open(Options.OutFile, 'w')
 print(Green + Bold + "Results about " + Options.Field + ":" + NC)
 json_data = json.loads(response.text)
 print_result(json_data)
 sys.stdout = sys.__stdout__
-if Options.OutFile != False:
-    f = open(Options.OutFile, 'r')
-    print f.read()
-    f.close()
-			for i in range(len(json_data)):
-				if 'Email' in json_data[i]["_source"]:
-					source = Red + "Email: " + Blue + json_data[i]["_source"]["Email"] + NC
-				elif 'User' in json_data[i]["_source"]:
-					source = Red + "User: " + Blue + json_data[i]["_source"]["User"] + NC
-				if 'Password' in json_data[i]["_source"]:
-					data = Red + "Password: " + Blue + json_data[i]["_source"]["Password"] + NC
-				elif 'PassHash' in json_data[i]["_source"]:
-					data = Red + "Hash: " + Blue + json_data[i]["_source"]["PassHash"] + NC
-
-				if json_data[i]["_score"] >= 4:
-					print("   " + Red + "Domain: " + Blue + json_data[i]["_source"]["Domain"] + NC + "\n" + "   " + source + "\n" + "   " + data + "\n")
-				
-			line = fp.readline()
-			cnt += 1
-else:
-
-	#INDIVIDUAL SEARCH
-	params = (
-		('q', sys.argv[1] + ":" + sys.argv[2]),
-		('num', '1'),
-		#('from', '200'),
-	)
-
-	response = requests.get('https://scylla.sh/search', headers=headers, params=params)
-
-	json_data = json.loads(response.text)
-
-	#This print is to get all data and test the output
-	#print(response.text.replace(',"_type":"_doc"},', '\n\n').replace('","', '"\n"'))
-
-	print(Green + Bold + "Results about " + sys.argv[2] + ":" + NC)
-
-	for i in range(len(json_data)):
-		if 'Email' in json_data[i]["_source"]:
-			source = Red + "Email: " + Blue + json_data[i]["_source"]["Email"] + NC
-		elif 'User' in json_data[i]["_source"]:
-			source = Red + "User: " + Blue + json_data[i]["_source"]["User"] + NC
-		if 'Password' in json_data[i]["_source"]:
-			data = Red + "Password: " + Blue + json_data[i]["_source"]["Password"] + NC
-		elif 'PassHash' in json_data[i]["_source"]:
-			data = Red + "Hash: " + Blue + json_data[i]["_source"]["PassHash"] + NC
-		if 'Domain' in json_data[i]:
-			domain = "Domain: " + Blue + json_data[i]["_source"]["Domain"] + NC + "\n"
-		else:
-			domain = ""
-
-		if json_data[i]["_score"] >= 4:
-			print("   " + Red + domain + "   " + source + "\n" + "   " + data + "\n")
+if Options.OutFile is not False:
+	f = open(Options.OutFile, 'r')
+	print(f.read())
+	f.close()
