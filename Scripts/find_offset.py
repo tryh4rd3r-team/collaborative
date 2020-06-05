f54a0c9d0dfa0bea85c08218519a91e379d014d9
root <root@localhost.localdomain>
2020-06-05 23:52:36 +0200

    Find_Offset

---
+++ Scripts/find_offset.py
@@ -0,0 +1,17 @@
+import subprocess
+
+len = raw_input("\nEnter the pattern lenght: ")
+
+p1 = subprocess.Popen(['/usr/share/metasploit-framework/tools/exploit/pattern_create.rb', '-l', len], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+pattern, err = p1.communicate()
+
+print("\nUse this pattern in GDB: \n" + pattern)
+
+eip = raw_input("Enter the $eip response address: ").replace("0x", "")
+
+p2 = subprocess.Popen(['/usr/share/metasploit-framework/tools/exploit/pattern_offset.rb', '-l', len, '-q', eip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+offset, err = p2.communicate()
+
+print(offset + "\n")
