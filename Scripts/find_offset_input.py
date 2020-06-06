e2ecf0254e7ef1dbb13f9996b673793674cb02e8
Rival420 <51548322+Rival420@users.noreply.github.com>
2020-06-06 15:04:38 -0400

    Update find_offset_input.py

--- Scripts/find_offset_input.py
+++ Scripts/find_offset_input.py
@@ -1,11 +1,24 @@
 import os, re, sys
+import argparse
 
-len = raw_input("\nEnter the pattern length: ")
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--length", dest="pattern_length", help="Pattern Length.", type=int)
+    parser.add_argument("-p", "--path", dest="fullpath", help="absolute path to binary")
+    options = parser.parse_args()
+    if not options.pattern_length:
+        parser.error("[-] Please specify the length of the payload. use -h for help.")
+    if not options.fullpath:
+        parser.error("[-] Please specify the full absoltu path to the binary. use -h for help.")
+    return options
 
-filename = raw_input("\nEnter the absolute binary pathname: ")
 
+
+
+options = get_arguments()
+len = options.length
+filename = options.fullpath
 execute = os.popen('cyclic ' + len + '|' + filename)
-
 address = os.popen('dmesg | tail -n 2 | head -n 1 | cut -d ' ' -f 6').read()
 
 #address = re.search('%s(.*)%s' % ('segfault at ', ' ip '), err).group(1)
