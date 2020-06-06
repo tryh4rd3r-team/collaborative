8b9cee0b9e88c8d4d6bca3f1331f6a76bc8fe3b4
Rival420 <51548322+Rival420@users.noreply.github.com>
2020-06-06 15:03:00 -0400

    Update find_offset.py

--- Scripts/find_offset.py
+++ Scripts/find_offset.py
@@ -1,6 +1,23 @@
 import subprocess
+import argparse
 
-len = raw_input("\nEnter the pattern length: ")
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--length", dest="pattern_length", help="Pattern Length.", type=int)
+    parser.add_argument("-e", "--eip", dest="eip", help="hex value EIP")
+    options = parser.parse_args()
+    if not options.pattern_length:
+        parser.error("[-] Please specify the length of the payload. use -h for help.")
+    if not options.fullpath:
+        parser.error("[-] Please specify the full absoltu path to the binary. use -h for help.")
+    return options
+
+
+
+
+options = get_arguments()
+len = options.length
+filename = options.fullpath
 
 p1 = subprocess.Popen(['/usr/share/metasploit-framework/tools/exploit/pattern_create.rb', '-l', len], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -8,7 +25,7 @@ pattern, err = p1.communicate()
 
 print("\nUse this pattern in GDB: \n" + pattern)
 
-eip = raw_input("Enter the $eip response address: ").replace("0x", "")
+eip = options.eip
 
 p2 = subprocess.Popen(['/usr/share/metasploit-framework/tools/exploit/pattern_offset.rb', '-l', len, '-q', eip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
