a7e619382d4d06edad3280512718efe6db212fd5
Rival420 <51548322+Rival420@users.noreply.github.com>
2020-06-06 15:03:46 -0400

    Update find_offset_argument.py

--- Scripts/find_offset_argument.py
+++ Scripts/find_offset_argument.py
@@ -1,8 +1,24 @@
 import os, re, sys
+import argparse
 
-len = raw_input("\nEnter the pattern length: ")
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--length", dest="pattern_length", help="Pattern Length.", type=int)
+    parser.add_argument("-p", "--path", dest="fullpath", help="absolute path to binary")
+    options = parser.parse_args()
+    if not options.pattern_length:
+        parser.error("[-] Please specify the length of the payload. use -h for help.")
+    if not options.fullpath:
+        parser.error("[-] Please specify the full absoltu path to the binary. use -h for help.")
+    return options
 
-filename = raw_input("\nEnter the absolute binary pathname: ")
+
+
+
+options = get_arguments()
+len = options.length
+
+filename = options.fullpath
 
 execute = os.popen(filename + '$(cyclic ' + len + ')')
 
@@ -15,4 +31,5 @@ offset = os.popen('cyclic -l 0x' + address).read()
 sys.stdout.write("\033[F")
 sys.stdout.write("\033[K")
 
+print("\nThe offset is: " + offset)
 print("\nThe offset is: " + offset)
